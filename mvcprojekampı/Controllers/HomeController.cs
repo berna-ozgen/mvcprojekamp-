351caf5c1fca5697a68944fad434e8a96f53b638
BERNA ÖZGEN <brnozgen@hotmail.com>
2021-04-29 01:43:22 +0300

    1.ödev

---
+++ mvcprojekampı/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcprojekampı.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()//hakkımda metodu
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()//iletişim metodu
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Test()
+        {
+            return View();
+        }
+ 
+    }
+}
---
+++ mvcprojekampı/Controllers/TEstController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcprojekampı.Controllers
+{
+    public class TEstController : Controller
+    {
+        // GET: TEst
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
